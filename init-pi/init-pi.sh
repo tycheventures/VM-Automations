c8348be82f35b7b7d04b4748f6e1cf03cb5b99c0
sohamjoshi8275 <74714812+sohamjoshi8275@users.noreply.github.com>
2021-02-20 17:52:49 +0530

    Create init-pi.sh

---
+++ init-pi/init-pi.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get upgrade
+apt install xinit openbox  python-qt4 python-configparser python-urllib3 x11-xserver-utils lxterminal freerdp2-x11 xscreensaver alsa-utils pulseaudio git arandr
