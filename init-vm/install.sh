e4161216389e599788e3e419fb79281d7d4e39d3
Makarand Mane <makarand@wpgenius.in>
2021-03-24 23:45:15 +0530

    Fix: removed wrong parameters
    Removed wrong parameters at wrong place like assume yes
    Move some command lines as per dependency.

--- init-vm/install.sh
+++ init-vm/install.sh
@@ -8,8 +8,8 @@ sudo apt autoremove --assume-yes
 #installation of xrdp
 sudo apt install xubuntu-desktop --assume-yes
 sudo apt install xrdp --assume-yes
-sudo adduser xrdp ssl-cert --assume-yes
-sudo systemctl restart xrdp --assume-yes
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
 
 #Dconf-editor
 sudo apt-get install dconf-editor --assume-yes
@@ -42,6 +42,8 @@ sudo apt-get install git --assume-yes
 
 #samba
 sudo apt install samba --assume-yes
+sudo service smbd restart
+sudo ufw allow samba
 
 #adobe reader
 sudo apt install gdebi-core libxml2:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386 libatk-adaptor:i386 --assume-yes
@@ -68,9 +70,9 @@ sudo apt-get install sublime-merge --assume-yes
 #Visual studio code
 sudo snap install --classic code # or code-insiders
 
-#wpcli
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
-wp --info
+#PFC- project folder creator
+mkdir ~/work
+wget -qO pfc https://raw.githubusercontent.com/tycheventures/vm-automations/main/pfc/pfc && sudo mv pfc /usr/bin/ && sudo chmod +x /usr/bin/pfc
 
 #webinoly
 wget -qO weby qrok.es/wy && sudo bash weby 3
@@ -88,9 +90,9 @@ sudo service nginx restart
 cd ~ && curl -sS https://getcomposer.org/installer -o composer-setup.php && HASH=`curl -sS https://composer.github.io/installer.sig` && php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php');   } echo PHP_EOL;" 
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
+#wpcli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
+wp --info
+
 #ip address
 ifconfig
-
-#PFC- project folder creator
-mkdir ~/work
-wget -qO pfc https://raw.githubusercontent.com/tycheventures/vm-automations/main/pfc/pfc && sudo mv pfc /usr/bin/ && sudo chmod +x /usr/bin/pfc
