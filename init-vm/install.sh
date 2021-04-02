e26e37d91b8ca9be377fcedac44eef66d82d2466
Makarand Mane <makarand@wpgenius.in>
2021-04-02 20:29:17 +0530

    Major fixes & arrangements
    Rearrangement of codes. 
    minimize lines
    Fix some issues

--- init-vm/install.sh
+++ init-vm/install.sh
@@ -1,21 +1,18 @@
-mkdir -p init-temp && cd init-temp/
+mkdir -p /tmp/init-temp && cd /tmp/init-temp/
 
 #cheking update
 sudo apt-get update --assume-yes
 sudo apt-get upgrade --assume-yes
-sudo apt install net-tools unzip curl qemu-guest-agent --assume-yes
+sudo apt install autoremove net-tools unzip curl qemu-guest-agent --assume-yes
 sudo systemctl start qemu-guest-agent
-sudo apt autoremove --assume-yes
 
 #installation of xrdp
-sudo apt install xubuntu-desktop --assume-yes
-sudo apt install xrdp --assume-yes
+sudo apt install xubuntu-desktop xrdp --assume-yes
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
 
 #popupfix for rdp
-wget https://raw.githubusercontent.com/tycheventures/VM-Automations/main/init-vm/45-allow-colord.pkla
-mv 45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/
+wget https://raw.githubusercontent.com/tycheventures/VM-Automations/main/init-vm/45-allow-colord.pkla && sudo mv 45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/
 
 #genome shell extension
 sudo apt install gnome-shell-extensions --assume-yes
@@ -28,36 +25,30 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink true
 dconf write /org/gnome/shell/extensions/dash-to-dock/force-straight-corner true
 dconf write /org/gnome/shell/extensions/dash-to-dock/autohide true
 
-#chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb --assume-yes
-
-#filezilla
-sudo apt-get install filezilla --assume-yes
-
-#kruler
-sudo apt-get install -y kruler --assume-yes
-
-#gimp
-sudo snap install gimp
-
-#opera
-sudo snap install opera
-
-#skype
-wget https://go.skype.com/skypeforlinux-64.deb
-sudo apt install ./skypeforlinux-64.deb --assume-yes
-
-#slack
-sudo snap install slack --classic
-
-#git
-sudo apt-get install git --assume-yes 
-
 #samba
 sudo apt install samba --assume-yes
 sudo service smbd restart
 sudo ufw allow samba
+
+#git, filezilla, kruler
+sudo apt-get install git filezilla kruler --assume-yes
+
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb --assume-yes
+
+#opera
+sudo snap install opera
+
+#gimp
+sudo snap install gimp
+
+#slack
+sudo snap install slack --classic
+
+#skype
+wget https://go.skype.com/skypeforlinux-64.deb
+sudo apt install ./skypeforlinux-64.deb --assume-yes
 
 #adobe reader
 sudo apt install gdebi-core libxml2:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386 libatk-adaptor:i386 --assume-yes
@@ -70,25 +61,15 @@ sudo npm install -g npm
 sudo npm install -g brewer
 sudo npm install -g grunt-cli
 
-#sublime text
+#sublime text & sublime merge
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https --assume-yes	
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
-
-#sublime merge
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https --assume-yes
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-merge --assume-yes
+sudo apt-get install sublime-text sublime-merge --assume-yes
 
 #Visual studio code
 sudo snap install --classic code # or code-insiders
-
-#PFC- project folder creator
-mkdir ~/work
-wget -qO pfc https://raw.githubusercontent.com/tycheventures/vm-automations/main/pfc/pfc && sudo mv pfc /usr/bin/ && sudo chmod +x /usr/bin/pfc
 
 #webinoly
 wget -qO weby qrok.es/wy && sudo bash weby 3
@@ -103,7 +84,7 @@ sudo httpauth -add=[tyche,net]
 #composer
 sudo apt install php-cli php-zip php-curl php-mbstring --assume-yes
 sudo service nginx restart
-cd ~ && curl -sS https://getcomposer.org/installer -o composer-setup.php && HASH=`curl -sS https://composer.github.io/installer.sig` && php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php');   } echo PHP_EOL;" 
+curl -sS https://getcomposer.org/installer -o composer-setup.php && HASH=`curl -sS https://composer.github.io/installer.sig` && php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php');   } echo PHP_EOL;" 
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 #wpcli
@@ -112,3 +93,8 @@ wp --info
 
 #ip address
 ifconfig
+
+#PFC- project folder creator
+mkdir -p ~/work
+wget -qO pfc https://raw.githubusercontent.com/tycheventures/vm-automations/main/pfc/pfc && sudo mv pfc /usr/bin/ && sudo chmod +x /usr/bin/pfc
+echo "Start creating project folder & subfolders of project with command 'pfc domain.com'. Use projects domain name as project folder name"
